9df311d12e6cf2af583fc6bd6b347ee6a64357ad
abahirov <a.bahirov@student.neoversity.com.ua>
2026-07-30 03:17:00 +0300

    upd2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,22 +1,22 @@
-const menuButton = document.querySelector('[data-menu-button]');
-const mobileMenu = document.querySelector('[data-menu]');
-const menuLinks = mobileMenu.querySelectorAll('a, button');
+const menuButton = document.querySelector("[data-menu-button]");
+const mobileMenu = document.querySelector("[data-menu]");
+const menuLinks = mobileMenu.querySelectorAll("a, button");
 
 const toggleMenu = () => {
-  const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
+  const isExpanded = menuButton.getAttribute("aria-expanded") === "true";
 
-  menuButton.classList.toggle('is-open');
-  menuButton.setAttribute('aria-expanded', String(!isExpanded));
+  menuButton.classList.toggle("is-open");
+  menuButton.setAttribute("aria-expanded", String(!isExpanded));
 
-  mobileMenu.classList.toggle('is-open');
-  document.body.classList.toggle('menu-open');
+  mobileMenu.classList.toggle("is-open");
+  document.body.classList.toggle("menu-open");
 };
 
-menuButton.addEventListener('click', toggleMenu);
+menuButton.addEventListener("click", toggleMenu);
 
 menuLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    if (mobileMenu.classList.contains('is-open')) {
+  link.addEventListener("click", () => {
+    if (mobileMenu.classList.contains("is-open")) {
       toggleMenu();
     }
   });
